f9968d366b80c6fc567a97e1a30b6b74362aee81
Owen Davis <ogdtechnology@gmail.com>
2026-02-16 09:19:40 -0500

    Updated code
    - Overall updated design
    - Updated content
    - Cross device compatible

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 particlesJS("particles", 
 
-{"particles":{"number":{"value":6,"density":{"enable":true,"value_area":1000
-    }
-},
-"color":{"value":"#1b1e34"
+{"particles":{
+    "number":{
+        "value":6,
+        "density":{
+            "enable":true,
+            "value_area":1000
+        }
     },
-"shape":{"type":"polygon","stroke":{"width":0,"color":"#000"
+    "color":{
+        "value":"#1b1e34"
+        },
+        
+    "shape":{"type":"polygon","stroke":{"width":0,"color":"#000"
+        },
+
+    "polygon":{"nb_sides":6
+        },
+        
+    "image":{"src":"img/github.svg","width":100,"height":100
+        }
     },
-"polygon":{"nb_sides":6
+    "opacity":{
+        "value":0.3,
+        "random":true,
+        "anim":{
+            "enable":false,
+            "speed":1,
+            "opacity_min":0.1,
+            "sync":false
+        }
     },
-"image":{"src":"img/github.svg","width":100,"height":100
-    }
-},
-"opacity":{"value":0.3,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false
-    }
-},
-"size":{"value":160,"random":false,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false
-    }
-},
-"line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":2
-},
-"move":{"enable":true,"speed":8,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200
+    "size":{
+        "value":100,
+        "random":false,
+        "anim":{
+            "enable":true,
+            "speed":10,
+            "size_min":40,
+            "sync":false
+        }
+    },
+    "line_linked":{
+        "enable":false,
+        "distance":200,
+        "color":"#ffffff",
+        "opacity":1,
+        "width":2
+    },
+    "move":{
+        "enable":true,
+        "speed":8,
+        "direction":"none",
+        "random":false,
+        "straight":false,
+        "out_mode":"out",
+        "bounce":false,
+        "attract":{
+            "enable":false,
+            "rotateX":600,
+            "rotateY":1200
         }
     }
 },
